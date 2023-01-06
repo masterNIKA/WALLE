5363a02296c82e939b1bb7c0c7676964e10b4541
mariam-bit <mariam.dolidze.2@btu.edu.ge>
2023-01-06 23:14:01 +0400

    createaccountpage emailverification mobileverification

---
+++ app/src/main/java/com/example/walle/mobileverification.kt
@@ -0,0 +1,11 @@
+package com.example.walle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mobileverification : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mobile_verification)
+    }
+}
