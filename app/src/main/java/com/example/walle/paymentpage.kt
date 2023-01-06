0a81488792bc43848bdd751aba7fa16e1e65232f
masterNIKA <116298004+masterNIKA@users.noreply.github.com>
2023-01-06 23:36:40 +0400

    main page payment page

---
+++ app/src/main/java/com/example/walle/paymentpage.kt
@@ -0,0 +1,11 @@
+package com.example.walle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class paymentpage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_page)
+    }
+}
