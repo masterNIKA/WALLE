c89908565196252df77647eba53ccd6501a38d76
masterNIKA <nikolozi.gongladze.1@btu.edu.ge>
2023-01-07 21:12:43 +0400

    Added 3 pages

---
+++ app/src/main/java/com/example/walle/Activity_Payment_Successful.kt
@@ -0,0 +1,11 @@
+package com.example.walle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_Payment_Successful : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment_successful)
+    }
+}
